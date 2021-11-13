17a1bcfdf57eb148ea4bc196a9f52b1d314a3f8d
Johann Verlohren <63848540+Johann-vln@users.noreply.github.com>
2021-11-13 11:11:48 +0100

    Delete School_Website directory

--- School_Website/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="vp.php">Vertretungsplan</a></li>
-    <li><a href="contact.php">Kontakte</a></li>
-    <li><a href="imprint.php">Impressum</a></li>
-  </ul>
-  <p><a href="#">&copy;2019-2020 Gönn Dir Crew</a></p>
-</footer>
--- School_Website/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // Starting Session
-
-if(session_destroy()) // Destroying All Sessions
-{
-header("Location: index.php"); // Redirecting To Home Page
-}
-?>
